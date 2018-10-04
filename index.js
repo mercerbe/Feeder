0d36e93eeb20b8b88cfc576905e9d7633671606a
Admin <mercerbe@gmail.com>
2018-10-04 18:37:46 -0400

    add nodemon, keys, googleStrategy for API

--- index.js
+++ index.js
@@ -2,18 +2,31 @@
 const express = require("express");
 //import express -- es6 modules -- not on node yet
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oath20").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//keys object
+const keys = require("./config/keys");
 
 //define express app
 const app = express();
 
 //define passport --
 //console.developers.google.com
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 //routes
 
 // config for heroku
 // if in production -- use env
-const PORT = process.env.PORT || 4000;
-app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`>App running on port ${PORT}`));
