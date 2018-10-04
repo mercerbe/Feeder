aeb080144a4e8cd9d7130767585e773ad4e02f94
Admin <mercerbe@gmail.com>
2018-10-04 18:24:21 -0400

    add google keys

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 //express -- common js module
 const express = require("express");
 //import express -- es6 modules -- not on node yet
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oath20").Strategy;
 
 //define express app
 const app = express();
 
+//define passport --
+//console.developers.google.com
+passport.use(new GoogleStrategy());
+
 //routes
-app.get("/", (req, res) => {
-  res.send({ hello: "world" });
-});
 
 // config for heroku
 // if in production -- use env
